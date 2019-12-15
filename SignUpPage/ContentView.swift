63dcb35e8c4a8e3a41981cb97c77075e3f0713f2
Joel Joseph <joeljordanjoseph@gmail.com>
2019-12-15 16:27:27 -0700

    Initial Commit

---
+++ SignUpPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignUpPage
+//
+//  Created by Joel Joseph on 12/15/19.
+//  Copyright © 2019 Joel Joseph. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
